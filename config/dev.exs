be9d0e114093908fa7645435b8f83809740a6378
chasekaylee <chaseklee990@gmail.com>
2021-01-29 13:06:52 -0800

    Add missing quote

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,4 @@ import Config
 config :trader_bot,
   command_prefix: "!"
 
-  config :logger, :console, format: "$time [$level] $metadata$message\n
+config :logger, :console, format: "$time [$level] $metadata$message\n"
